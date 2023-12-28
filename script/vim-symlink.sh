6fc1c6e7a61d01c03b959792f97bcbc032c875d5
izzany <izzany@cdglobal.biz>
2023-12-28 16:27:50 +0800

    embedded repo fix

--- script/vim-symlink.sh
+++ script/vim-symlink.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ln -s ../vim/.vim $HOME/.vim &&
-ln -s ../vim/vim $HOME/vim &&
 ln -s ../vim/.viminfo $HOME/.viminfo &&
 ln -s ../vim.vimrc $HOME/.vimrc &&
 ln -s ../vim/.vimrc.plug $HOME/.vimrc.plug
