a714f36b617a0b37ae60ae3033b72ee4251684b0
izzany <izzany@cdglobal.biz>
2023-12-28 16:54:59 +0800

    minor fix

--- script/vim-symlink.sh
+++ script/vim-symlink.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ln -sf ../vim/.vim $HOME/.vim &&
 ln -sf ../vim/.viminfo $HOME/.viminfo &&
-ln -sf ../vim.vimrc $HOME/.vimrc &&
+ln -sf ../vim/.vimrc $HOME/.vimrc &&
 ln -sf ../vim/.vimrc.plug $HOME/.vimrc.plug
 
